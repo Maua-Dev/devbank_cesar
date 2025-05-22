db23469001ce7f9195888aa8321c92cf0d1acacc
CesarPinesi <cpinesi.devmaua@gmail.com>
2025-05-21 21:27:28 -0300

    teste da lógica de history 🔎

--- src/app/repo/transaction_repository_mock.py
+++ src/app/repo/transaction_repository_mock.py
@@ -1,13 +1,17 @@
 from typing import Dict, Optional, List, Tuple
 from ..entities.user import User
 from ..entities.trasaction import Transaction
+from ..enums.transaction_type_enum import TransactionTypeEnum
 
 class Trasaction_repository:
 
     transactions: Dict[int, Transaction]
 
     def __init__(self):
-        self.transactions = {}
+        self.transactions = {
+            1: Transaction(transaction_type = TransactionTypeEnum.DEPOSIT, value = 200.0, timestamp = 123.0, current_balance = 1000),  
+            1: Transaction(transaction_type = TransactionTypeEnum.WITHDRAW, value = 100.0, timestamp = 321.0, current_balance = 1000)         
+        }
     
     def create_transaction(self, transaction: Transaction):
         self.transactions[len(self.transactions.values) + 1] = transaction
