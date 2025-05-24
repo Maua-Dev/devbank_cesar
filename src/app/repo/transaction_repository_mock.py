f058d171a4987e7876471193377d5a0f11333a22
CesarPinesi <cpinesi.devmaua@gmail.com>
2025-05-24 12:50:17 -0300

    tentativa de correção da lógica de history

--- src/app/repo/transaction_repository_mock.py
+++ src/app/repo/transaction_repository_mock.py
@@ -5,19 +5,18 @@ from ..enums.transaction_type_enum import TransactionTypeEnum
 
 class Trasaction_repository:
 
-    all_transactions: List [Transaction]
-
-    def __init__(self):
-        self.transactions = {
+    all_transactions = [
         Transaction(transaction_type = TransactionTypeEnum.DEPOSIT, value = 200.0, timestamp = 123.0, current_balance = 1000.0),  
-        Transaction(transaction_type = TransactionTypeEnum.WITHDRAW, value = 100.0, timestamp = 321.0, current_balance = 1000.0)         
-        }
-    
-    def create_transaction(self, transaction: Transaction):
-        self.append(transaction)
+        Transaction(transaction_type = TransactionTypeEnum.WITHDRAW, value = 100.0, timestamp = 321.0, current_balance = 1000.0),
+        ]
+
+
+    def create_transaction(self, all_transactions: List, transaction: Transaction):
+        all_transactions.append(transaction)
+        return transaction
 
     def get_history(self) -> List[Transaction]:
-        return self.transactions.values()
+        return self.all_transactions.values()
 
 
 
